ab6d82dec0dd089f6a9a4ee2b448ac1a007f092e
shunqiziranrrww <462292607@qq.com>
2021-03-29 22:54:43 +0800

    自动化测试，第一次上传hao
    Signed-off-by: shunqiziranrrww <462292607@qq.com>

---
+++ hh.py
@@ -0,0 +1,14 @@
+class person:
+    name = "tom"
+    age = 20
+    abc = "eee"
+
+    def rrr(self):
+        print("吃饭")
+
+    def ooo(self):
+        print("睡觉")
+
+
+zhangsan = person()
+zhangsan.rrr()
